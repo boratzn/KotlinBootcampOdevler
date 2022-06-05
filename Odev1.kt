7c74eb7701f0448bc31c7dae5adcaaf945ea5a6c
Bora TÜZÜN <boratzn94@gmail.com>
2022-06-05 23:08:25 +0300

    Create Odev1.kt

---
+++ Odev1.kt
@@ -0,0 +1,49 @@
+fun main() {
+
+    var islemNo = 1
+    var ogrNo = 100
+    var kitapNo = 2
+    var aTarih = "1.06.2022"
+    var vTarih = "15.06.2022"
+
+    println("İşlem No : $islemNo")
+    println("Öğrenci No : $ogrNo")
+    println("Kitap No : $kitapNo")
+    println("Alış Tarihi : $aTarih")
+    println("Teslim Tarihi : $vTarih")
+
+    var ogrAd = "Bora"
+    var ogrSoyad = "TÜZÜN"
+    var cinsiyet = "E"
+    var dTarih = "12.12.1994"
+    var sinif = 4
+
+    println("Öğrenci Ad Soyad : $ogrAd $ogrSoyad")
+    println("Cinsiyet : $cinsiyet")
+    println("Doğum Tarihi : $dTarih")
+    println("Sınıf : $sinif")
+
+    var isbnNo = 12
+    var kitapAdı = "Genç Werther'in Acıları"
+    var sayfaSayisi = 126
+    var puan = 7
+
+    println("Isnbn No : $isbnNo")
+    println("Kitap Adı : $kitapAdı")
+    println("Sayfa Sayısı : $sayfaSayisi")
+    println("Puan : $puan")
+
+    var yazarNo = 1
+    var yazarAd = "Von"
+    var yazarSoyad = "Goethe"
+
+    println("Yazar No : $yazarNo")
+    println("Yazar Ad : $yazarAd")
+    println("Yazar Soyad : $yazarSoyad")
+
+    var turNo = 1
+    var turAdi = "Asd"
+
+    println("Tur No : $turNo")
+    println("Tur Adı : $turAdi")
+}
